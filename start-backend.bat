53bb421c2840c7bbe6479f89c9f91f4e9f983509
pradeepreddyaja <p.perumalla@ajacs.in>
2026-06-23 12:13:22 +0530

    Migrate backend to PostgreSQL, add Lombok, fix start script, update properties

--- start-backend.bat
+++ start-backend.bat
@@ -1,5 +1,3 @@
 @echo off
-set "JAVA_HOME=C:\Users\DELL\.jdks\ms-17.0.18"
-set "PATH=%JAVA_HOME%\bin;%PATH%"
-echo Starting PVR Prime Naturals Backend (CLEAN BUILD) with JAVA_HOME=%JAVA_HOME%
-"C:\Users\DELL\.m2\wrapper\dists\apache-maven-3.9.12-bin\5nmfsn99br87k5d4ajlekdq10k\apache-maven-3.9.12\bin\mvn.cmd" clean spring-boot:run
+echo Starting PVR Prime Naturals Backend (CLEAN BUILD)
+"C:\Users\DELL\.m2\wrapper\dists\apache-maven-3.9.12-bin\5nmfsn99br87k5d4ajlekdq10k\apache-maven-3.9.12\bin\mvn.cmd" -B clean spring-boot:run
