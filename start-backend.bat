667157ad686e6d9a93593d516cb32078f0b6d77e
pradeepreddyaja <p.perumalla@ajacs.in>
2026-06-23 16:00:23 +0530

    Fix: Set JAVA_HOME in start-backend.bat

--- start-backend.bat
+++ start-backend.bat
@@ -1,3 +1,4 @@
 @echo off
+set "JAVA_HOME=C:\Program Files\Java\jdk-25.0.2"
 echo Starting PVR Prime Naturals Backend (CLEAN BUILD)
 "C:\Users\DELL\.m2\wrapper\dists\apache-maven-3.9.12-bin\5nmfsn99br87k5d4ajlekdq10k\apache-maven-3.9.12\bin\mvn.cmd" -B clean spring-boot:run
